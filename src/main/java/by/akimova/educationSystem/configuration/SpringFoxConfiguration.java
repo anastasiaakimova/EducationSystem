e9778c5a703854eb35506f7742e219a503ef5516
Anastasia <anastasiyaaki171@gmail.com>
2022-01-18 18:57:58 +0300

    Write method openApi

--- src/main/java/by/akimova/educationSystem/configuration/SpringFoxConfiguration.java
+++ src/main/java/by/akimova/educationSystem/configuration/SpringFoxConfiguration.java
@@ -1,8 +1,8 @@
 package by.akimova.educationSystem.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class OpenApiConfiguration {
+public class SpringFoxConfiguration {
 
     /*@Bean
     public Docket createRestApi() {
@@ -28,31 +28,17 @@ public class OpenApiConfiguration {
                 .paths(PathSelectors.any())
                 .build();
     }
-
+*/
     @Bean
-    public OpenAPI springOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Spring API")
-                        .description("Spring shop sample application")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("Spring Wiki Documentation")
-                        .url("https://springshop.wiki.github.org/docs"));
-    }*/
-
-    @Bean
-    public OpenAPI customOpenAPI(@Value("desct") String appDesciption,
-                                 @Value("1.3.3") String appVersion) {
-
+    public OpenAPI customOpenAPI(@Value("1.3.3") String appVersion) {
         return new OpenAPI()
                 .info(new Info()
-                        .title("sample application API")
+                        .title("educationSystem")
                         .version(appVersion)
-                        .description(appDesciption)
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0")
-                                .url("http://springdoc.org")));
-
+                        .contact(
+                                new Contact()
+                                        .name("Nasta")
+                        )
+                );
     }
 }
