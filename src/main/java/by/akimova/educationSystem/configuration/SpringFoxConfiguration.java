dc3885d929de44db71302774a62b29978bcde717
Anastasia <anastasiyaaki171@gmail.com>
2022-01-18 18:48:24 +0300

    Edit springFox annotations, delete exceptions catching

--- src/main/java/by/akimova/educationSystem/configuration/SpringFoxConfiguration.java
+++ src/main/java/by/akimova/educationSystem/configuration/SpringFoxConfiguration.java
@@ -1,34 +1,58 @@
-package by.akimova.educationSystem;
+package by.akimova.educationSystem.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 /*****************************************************************************************
- * Configuration for Swagger
- *
  * @author Akimova Anastasia
  * @version 1.0
  *
- * Copyright (c) 2022.
+ * Copyright (c) 2022-2022.
  ****************************************************************************************/
 
 @Configuration
-//@EnableSwagger2
 @RequiredArgsConstructor
-public class SpringFoxConfig {
+public class OpenApiConfiguration {
 
-    @Bean
+    /*@Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Spring API")
+                        .description("Spring shop sample application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Spring Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }*/
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("desct") String appDesciption,
+                                 @Value("1.3.3") String appVersion) {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("sample application API")
+                        .version(appVersion)
+                        .description(appDesciption)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0")
+                                .url("http://springdoc.org")));
+
     }
 }
--- src/main/java/by/akimova/educationSystem/controller/UserController.java
+++ src/main/java/by/akimova/educationSystem/controller/UserController.java
@@ -1,9 +1,13 @@
 package by.akimova.educationSystem.controller;
 
-import by.akimova.educationSystem.exception.EntityNotFoundException;
-import by.akimova.educationSystem.model.User;
 import by.akimova.educationSystem.service.UserService;
 import by.akimova.educationSystem.service.dto.UserDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -25,32 +29,33 @@ import java.util.List;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "User", description = "REST controller user connected requests")
 public class UserController {
 
     private final UserService userService;
 
     /**
      * The method add new user.
-     * <p>
-     * This is item with its information and body.
      *
+     * @param userDto This is dto with all information and body.
      * @return response with body of created user and status ok.
      */
 
-   /* @ApiOperation (value = "Create user", notes = "Create user based on DTO object")
-    @ApiImplicitParam (name = "userDto", value = "userDto", required = true, dataType = "UserDto")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(code = 200, message = "User crated"),
-                    @ApiResponse(code = 400, message = "Bad request"),
-                    @ApiResponse(code = 500, message = "Something went wrong")
-            })*/
+
     @PostMapping("")
-    public ResponseEntity<User> addUser(@RequestBody UserDto userDto) {
-        User savedUser;
+    @Operation(
+            summary = "Create user",
+            description = "Create user based on DTO object",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User crated",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content)
+            })
 
-        savedUser = userService.save(userDto);
-
+    public ResponseEntity<UserDto> create(@RequestBody UserDto userDto) {
+        UserDto savedUser = userService.save(userDto);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
@@ -60,15 +65,17 @@ public class UserController {
      * @return ResponseEntity with list of users and status ok.
      */
     @GetMapping
-  /*  @ApiOperation (value = "Get list of users", notes = "Get list of users")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(code = 200, message = "Getting all users"),
-                    @ApiResponse(code = 400, message = "Bad request"),
-                    @ApiResponse(code = 500, message = "Something went wrong")
-            })*/
-    public ResponseEntity<List<User>> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
+    @Operation(
+            summary = "Get list of users",
+            description = "Get list of users",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Getting list of users",
+                            content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content)
+            })
+    public ResponseEntity<List<UserDto>> getAll() {
+        return ResponseEntity.ok(userService.getAll());
     }
 
     /**
@@ -77,24 +84,25 @@ public class UserController {
      * @param id This is id of the person to be found.
      * @return ResponseEntity with found user and status ok.
      */
-    /*@ApiOperation(value = "Get information about user", notes = "Get information about user by id")
-    @ApiImplicitParam(name = "id", value = "User ID", required = true, dataType = "Long", paramType = "path")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(code = 200, message = "Getting user"),
-                    @ApiResponse(code = 400, message = "Bad request"),
-                    @ApiResponse(code = 500, message = "Something went wrong")
-            })*/
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable(value = "id") Long id) {
-        User user;
-        try {
-            user = userService.getById(id);
-        } catch (EntityNotFoundException e) {
-            log.error("IN UserController getUserById - user by id {} not found", id);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(user, HttpStatus.OK);
+    @Operation(
+            summary = "Get user by id",
+            description = "Get information about user by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Getting user",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content)
+            })
+    public ResponseEntity<?> getById(@PathVariable(value = "id")
+                                     @Parameter(description = "user's id",
+                                             schema = @Schema(
+                                                     type = "Long"))
+                                             Long id) {
+        UserDto userDto = userService.getById(id);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
     }
 
     /**
@@ -104,34 +112,49 @@ public class UserController {
      * @return ResponseEntity with found user and status ok.
      */
     @GetMapping("mail/{mail}")
-    ResponseEntity<?> getUserByMail(@PathVariable(value = "mail") String mail) throws EntityNotFoundException {
-        User user;
-        try {
-            user = userService.findByMail(mail).orElseThrow(
-                    () -> new EntityNotFoundException("User doesn't exists!"));
-        } catch (EntityNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        return ResponseEntity.ok(user);
+    @Operation(
+            summary = "Get user by mail",
+            description = "Get information about user by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Getting user",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content)
+            })
+    ResponseEntity<?> getByMail(@PathVariable(value = "mail")
+                                @Parameter(description = "user's mail",
+                                        schema = @Schema(
+                                                type = "String")) String mail) {
+        UserDto userDto = userService.findByMail(mail);
+        return ResponseEntity.ok(userDto);
     }
 
     /**
      * The method update item.
      *
-     * @param id   This is user's id which should be updated.
-     * @param user This is new body for user which should be updated.
+     * @param id      This is user's id which should be updated.
+     * @param userDto This is new body for user which should be updated.
      * @return response with body of updated user and status ok.
      */
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateUser(@PathVariable(value = "id") Long id, @RequestBody UserDto user) {
-        User updatedUser;
-        try {
-            updatedUser = userService.updateUser(id, user);
-        } catch (EntityNotFoundException e) {
-            log.error("IN UserController updateUser - user by id {} not found", id);
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @Operation(
+            summary = "Update user",
+            description = "Update user's information",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Getting updated user",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content)
+            })
+    public ResponseEntity<?> update(@PathVariable(value = "id")
+                                    @Parameter(description = "user's id",
+                                            schema = @Schema(
+                                                    type = "Long"))
+                                            Long id,
+                                    @RequestBody UserDto userDto) {
+        var updatedUser = userService.update(id, userDto);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
@@ -142,8 +165,17 @@ public class UserController {
      * @return response status no_content.
      */
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable(value = "id") Long id) {
-        userService.deleteUserById(id);
+    @Operation(
+            summary = "Delete user",
+            description = "Delete user and all his information",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "user deleted",
+                            content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "Something went wrong", content = @Content)
+            })
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+        userService.deleteById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
