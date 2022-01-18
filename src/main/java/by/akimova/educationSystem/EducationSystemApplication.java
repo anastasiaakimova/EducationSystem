44556448f2bfb3af4039fb2a9bc7b914521688af
Anastasia <anastasiyaaki171@gmail.com>
2022-01-18 18:58:29 +0300

    Add springfox annotation

--- src/main/java/by/akimova/educationSystem/EducationSystemApplication.java
+++ src/main/java/by/akimova/educationSystem/EducationSystemApplication.java
@@ -1,15 +1,14 @@
 package by.akimova.educationSystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableSwagger2
+@OpenAPIDefinition
 public class EducationSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EducationSystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EducationSystemApplication.class, args);
+    }
 }
