5dc5185070ed3bd8a2a121175a3887782eae1c05
Anastasia <anastasiyaaki171@gmail.com>
2022-01-21 18:23:22 +0300

    Add permissions in role

--- src/main/java/by/akimova/educationSystem/model/Role.java
+++ src/main/java/by/akimova/educationSystem/model/Role.java
@@ -1,4 +1,10 @@
 package by.akimova.educationSystem.model;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /*****************************************************************************************
  * @author Akimova Anastasia
  * @version 1.0
@@ -7,6 +13,22 @@ package by.akimova.educationSystem.model;
  ****************************************************************************************/
 
 public enum Role {
-    USER,
-    ADMIN;
+    USER(Set.of(Permission.USER_READ)),
+    ADMIN(Set.of(Permission.USER_READ, Permission.USER_WRITE));
+
+    private final Set<Permission> permissionSet;
+
+    Role(Set<Permission> permissionSet) {
+        this.permissionSet = permissionSet;
+    }
+
+    public Set<Permission> getPermissionSet() {
+        return permissionSet;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissionSet().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
