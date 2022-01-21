8e7ccb51ef9a259b8fb491eef623774d99488a9b
Anastasia <anastasiyaaki171@gmail.com>
2022-01-21 18:24:18 +0300

    Add security configuration

---
+++ src/main/java/by/akimova/educationSystem/configuration/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+package by.akimova.educationSystem.configuration;
+
+import by.akimova.educationSystem.security.JwtConfigurer;
+import by.akimova.educationSystem.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/*****************************************************************************************
+ * Security configuration class for JWT based Spring Security application.
+ *
+ *  @author Akimova Anastasia
+ * @version 1.0
+ *
+ * Copyright (c) 2022.
+ ****************************************************************************************/
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public SecurityConfiguration(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login").permitAll()
+                .antMatchers("/users").permitAll()
+                .antMatchers(HttpMethod.PUT, "/users/**").fullyAuthenticated()
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
+
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+
+    }
+
+    @Bean
+    protected BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+}
